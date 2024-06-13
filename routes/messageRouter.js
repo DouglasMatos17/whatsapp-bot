5f602e7b5556d441a5db1252e76474f841d4c99f
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-06-13 10:19:35 -0300

    fix

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -7,18 +7,8 @@ router.get("/", (req, res) => {
   res.send("Olá mundo!!");
 });
 
-router.get("/qr", async (req, res) => {
-  const qrCode = getQRCode();
-  if (qrCode) {
-    try {
-      const qrImage = await qrcode.toDataURL(qrCode);
-      res.status(200).send(`<img src="${qrImage}" alt="QR Code"/>`);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to generate QR code image" });
-    }
-  } else {
-    res.status(404).json({ message: "QR code not generated yet" });
-  }
+app.get("/qr", async (req, res) => {
+  const qrCode = await generateQRCode();
+  res.send(qrCode);
 });
-
 module.exports = router;
