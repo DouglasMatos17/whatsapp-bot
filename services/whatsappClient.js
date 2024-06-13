49ab04d440151b647ea20efb3c446fe3c50debb8
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-06-13 08:32:54 -0300

    deployment

--- services/whatsappClient.js
+++ services/whatsappClient.js
@@ -1,6 +1,9 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
   webVersionCache: {
     type: "remote",
     remotePath:
